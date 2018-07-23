66645d88a98f97182602d47214bf90a1006c8098
Robert Dey <dey@kowalzik.biz>
2018-07-23 15:15:20 +0200

    add spring typescript services plugin

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -4,9 +4,13 @@ import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.api.v1.model.CategoryListDTO;
 import guru.springfamework.services.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.leandreck.endpoints.annotations.TypeScriptEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@TypeScriptEndpoint
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(CategoryController.BASE_URL)
@@ -16,13 +20,13 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public CategoryListDTO getAllCategories() {
         return new CategoryListDTO(categoryService.getAllCategories());
     }
 
-    @GetMapping("{name}")
+    @RequestMapping(name = "{name}", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public CategoryDTO getCategoryByName(@PathVariable String name) {
         return categoryService.getCategoryByName(name);
--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -4,9 +4,11 @@ import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.leandreck.endpoints.annotations.TypeScriptEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@TypeScriptEndpoint
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
