af12ccdf6ee9066a6768f58bc72a446aad699d9d
Robert Dey <dey@kowalzik.biz>
2018-07-25 10:43:59 +0200

    add gender to example customers

--- src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,5 +1,6 @@
 package guru.springfamework.api.v1.model;
 
+import guru.springfamework.domain.Gender;
 import lombok.Data;
 
 @Data
--- src/main/java/guru/springfamework/bootstrap/CustomerBootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/CustomerBootstrap.java
@@ -2,6 +2,7 @@ package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
 import guru.springfamework.domain.Customer;
+import guru.springfamework.domain.Gender;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,18 +25,22 @@ public class CustomerBootstrap implements CommandLineRunner {
         Customer susan = new Customer();
         susan.setFirstName("Susan");
         susan.setLastName("Tanner");
+        susan.setGender(Gender.FEMALE);
 
         Customer joe = new Customer();
         joe.setFirstName("Joe");
         joe.setLastName("Newman");
+        joe.setGender(Gender.MALE);
 
         Customer michael = new Customer();
         michael.setFirstName("Michael");
         michael.setLastName("Lachappele");
+        michael.setGender(Gender.MALE);
 
         Customer david = new Customer();
         david.setFirstName("David");
         david.setLastName("Winter");
+        david.setGender(Gender.MALE);
 
         customerRepository.save(susan);
         customerRepository.save(joe);
